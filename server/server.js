bbc800e9e500ea6f85094f4cb2f1e0e8d8f053da
Alison Butcher <alisonkbutcher@gmail.com>
2018-08-31 09:44:12 +1000

    Added import for body parser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const fs = require('fs');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require('./routes/auth.js')(app,fs);
 require('./routes/register.js')(app,fs);
