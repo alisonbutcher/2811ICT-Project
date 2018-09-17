50899fc6bc91ddfa92e0391046d33e078eb09d1b
Alison Butcher <alisonkbutcher@gmail.com>
2018-09-17 10:22:03 +1000

    Login is now working correctly and returns composite JSON object with user details and accessLevel

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -3,16 +3,6 @@ var mongoose = require('mongoose'),
   Login = mongoose.model('User'),
   UserRole = mongoose.model('UserRole');
 
-// exports.list_all_users = function(req, res) {
-//   Login.find({}, function(err, user) {
-//     if (err)
-//       res.send(err);
-//     res.json(user);
-//   });
-// };
-
-
-
 exports.login = function(req, res) {
     // Build Query
     let query = JSON.parse('{"name": "' + req.body.name + '", "password": "' + req.body.password + '"}');
@@ -20,70 +10,39 @@ exports.login = function(req, res) {
     let u = null;
 
     // Perform user Query 
-    //TODO: For some reason the mongoose findOne query returns nothing so use find() instead
         Login.find(query, 'name email', function(err, user) {
             if (err) 
                 res.send(err);
 
             // If there is data the username and password are correct
             if (user.length > 0) {
-                // res.json(user);
-
-                console.log(user);
                 u = user;
             } else {
                 res.json({ message: 'Username or password Incorrect'});
         }
 
         if (u != null) {
-            console.log("we have a user object");
             // Build user role query
             let rolequery = JSON.parse('{"userId": "' + u[0]._id + '"}');
-            UserRole.find(rolequery, 'accessLevel', function(error, userRole) {
-                console.log(userRole.length);
+
+            // Perform query on user-roles
+            UserRole.find(rolequery, 'accessLevel',{ lean: true }, function(error, userRole) {
+
+                // Is role found
                 if (userRole.length > 0) {
-                    console.log(JSON.stringify(userRole[0].accessLevel));
+
                     // Build output json
-                    // TODO: We cant access elements in the returned userRole for some reason.
-                    let out = '{"name": "' + u[0].name + '", "email": "' + u[0].email + '", "accessLevel": "' + userRole[0].accessLevel + '"}';
-                    console.log(out);
-                    res.json(userRole + u); // TODO: When the above is fixed remove this
+                    let out = JSON.parse('{"name": "' + u[0].name + '", "email": "' + u[0].email + '", "accessLevel": "' + userRole[0].accessLevel + '"}');
+                    
+                    // Return user and role access level object
+                    res.json(out);
+
                 } else {
+
                     res.json({ message: 'user found but no role for user not found'});
                 }
             })
         }
     });
-    
-    // Login.find(req.body, function(err, user) {
-    // if (err)
-    //   res.send(err);
-    // // Validate user
-    // console.log(user);
-    // console.log(req.body.name);
-    // if (req.body == user.name) {
-    //     console.log("user: " + user.name);
-    //     res.json(user);
-    // } else {
-    //     res.json({ message: 'Login Unsuccessful'});
-    // }
-//   });
 };
 
-// exports.update_a_user = function(req, res) {
-//   User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true}, function(err, user) {
-//     if (err)
-//       res.send(err);
-//     res.json(user);
-//   });
-// };
-
-// exports.delete_a_user = function(req, res) {
-//   User.deleteOne({
-//     _id: req.params.userId
-//   }, function(err, user) {
-//     if (err)
-//       res.send(err);
-//     res.json({ message: 'User successfully deleted' });
-//   });
-// };
