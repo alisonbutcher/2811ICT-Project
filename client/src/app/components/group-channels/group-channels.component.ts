84363d4000a88d91e351ef346ed3d493f8fd6993
Alison Butcher <alisonkbutcher@gmail.com>
2018-09-24 12:50:42 +1000

    added manage Group-Channels as per previous master details for group-users and channel-users

--- client/src/app/components/group-channels/group-channels.component.ts
+++ client/src/app/components/group-channels/group-channels.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupsService } from '../../services/groups.service';
+import { ChannelsService } from '../../services/channels.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-group-channels',
@@ -7,9 +10,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupChannelsComponent implements OnInit {
 
-  constructor() { }
+  private nonGroupchannels;
+  private groups;
+  private groupChannels = [];
+  private channels;
+  private selectedGroup;
+
+  constructor(private groupService: GroupsService, private channelService: ChannelsService) { }
 
   ngOnInit() {
+    this.getGroups();
+    this.getChannels();
+  }
+
+  selectGroup(group) {
+    this.selectedGroup = group;
+    this.getChannelsInGroup(group);
+  }
+
+  getGroups() {
+    this.groupService.getGroups().subscribe(
+      data => { this.groups = data; },
+      err => console.error(err),
+      () => console.log('done loading groups')
+    );
+  }
+
+  getChannels() {
+    this.channelService.getChannels().subscribe(
+      data => {
+        this.channels = data;
+      },
+      err => console.error(err),
+      () => console.log('done loading channels')
+    );
+  }
+
+  getChannelsInGroup(group) {
+    this.groupChannels = group.channels;
+  }
+
+  addChannelToGroup(channel) {
+    let str: string = '';
+    str = '{ "channelname": "' + channel.name + '" }';
+    console.log((str));
+    this.selectedGroup.channels.push(JSON.parse(str));
+    console.log(this.selectedGroup);
+
+    this.groupService.updateGroupByName(this.selectedGroup).subscribe(
+      data => {
+        this.getGroups();
+        return true;
+      },
+      error => {
+        console.error('Error saving group');
+      }
+    );
+  }
+
+  removeChannelFromGroup(channel) {
+    console.log('Remove channel from group: ' + channel.channelname);
+    this.selectedGroup.channels.pop(channel.channelname);
+    this.groupService.updateGroupByName(this.selectedGroup).subscribe(
+      data => {
+        this.getGroups();
+        return true;
+      },
+      error => {
+        console.error('Error saving group');
+      }
+    );
   }
 
 }
