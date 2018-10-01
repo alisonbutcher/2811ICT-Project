7973105cb6890432ca7f17f99c9be8f5864c6993
Alison Butcher <alisonkbutcher@gmail.com>
2018-10-01 15:54:16 +1000

    tidied up the code in socket.js on server side

--- server/socket.js
+++ server/socket.js
@@ -1,21 +1,15 @@
-
 // Get Chat Database
 let db = require('./controllers/chat.controller');
 
-module.exports = function(app, io){
+module.exports = function (app, io) {
     console.log("Server sockets initialised");
 
-    // TODO: Link database to history
-
-   // Websocket connection
+    // Websocket connection
     io.on('connection', (socket) => {
-        var chathistory;
-
-        console.log('user connected');
 
         // Separate chat room selection (channel)
         socket.on('changeRoom', (room) => {
-            
+
             // Leave old room
             socket.leave(socket.room);
 
@@ -26,25 +20,24 @@ module.exports = function(app, io){
             let m = JSON.parse('{ "channelname": "' + room + '", "username": "CHAT-SERVER", "msg": "Welcome to ' + room + '" }');
             socket.emit('UpdateChat', m);
 
+            // Store room
             socket.room = room;
 
-            console.log('Joined ' + room);
-            
             // Get history from db
-            db.read(room, function(data) {
-                chathistory = data;
-                // console.log(chathistory);
+            db.read(room, function (data) {
 
-            // Send each chathistory message one by one to socket
-            Object.keys(chathistory).forEach(function(key) {
-                socket.emit('UpdateChat', chathistory[key]);
-            });
-                
-                // socket.emit('UpdateChat', chathistory);
+                // Loop over each historical chat message returned from db
+                Object.keys(data).forEach(function (key) {
+
+                    // For each message emit to channel
+                    socket.emit('UpdateChat', data[key]);
+
+                });
+
             });
         });
 
-        
+
         // Disconnect
         socket.on('disconnect', () => {
             console.log('user disconnected');
@@ -54,24 +47,12 @@ module.exports = function(app, io){
         // Room specific messages
         socket.on('sendMessage', (message) => {
 
-            // build emit message
-            // let e = JSON.parse('{ "type": "message", "text": "[' + message.username + '] ' + message.msg + '" }');
-
             // Emit to channel
             io.sockets.in(socket.room).emit('UpdateChat', message);
 
-            // io.sockets.in(socket.room).emit('updateChat', {type:'message', text: message});
-
-            // // Format message for save to db
-            // let m = JSON.parse('{ "channelname": "' + message.channelname + '", "username": "' +
-            //     message.username + '", "msg": "' + message.msg + '" }');
-            
             // Write message to database
-            db.write(message, (data) => {
-                // console.log('pushed: ' + data);
-            });
+            db.write(message, (data) => {});
 
-            console.log('Room: ' + socket.room + ', ' + message);
         });
     });
 }
